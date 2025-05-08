93c6a2c3563c0fee872d2d8c8230bbf82e1d715f
linxin388 <2323406443@qq.com>
2025-05-08 22:37:11 +0800

    Update server.py
    Server-side code. The server listens on the port. A dictionary for storing tuple Spaces in the form of key-value pairs. Total number of client connections.

--- server.py
+++ server.py
@@ -2,3 +2,14 @@ import socket
 import threading
 import time
 from collections import defaultdict
+
+class TupleSpaceServer:
+    def __init__(self, port):
+        self.port = port
+        self.tuple_space = {}
+        self.client_count = 0
+        self.operation_count = 0
+        self.read_count = 0
+        self.get_count = 0
+        self.put_count = 0
+        self.error_count = 0
